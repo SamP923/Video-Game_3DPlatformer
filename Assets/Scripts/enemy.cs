050c2e71c82531b44f537781a8ce27f45203b50e
Sam P <fire.the.cannon.923@gmail.com>
2019-03-06 22:57:16 +0000

    Added coin/platform grouped prefabs, player movement/jumping

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+	public float speed = 10f;
+	public float rangeY = 1.5f;
+	public int direction = 1;
+	Vector3 initialPosition;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float movementY = speed * Time.deltaTime * direction;
+		float newY = transform.position.y + movementY;
+
+		if (Mathf.Abs (newY - initialPosition.y) > rangeY) {
+			direction *= -1;
+		} else {
+			transform.position += new Vector3(0, movementY, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	public float walkSpeed;
+	public float jumpSpeed;
+
+	Vector3 movement;
+
+	Rigidbody playerRB;
+	bool canJump;
+	bool isGrounded = true;
+	// Use this for initialization
+	void Start () {
+		walkSpeed = 5f;
+		playerRB = GetComponent<Rigidbody>();
+		canJump = true;
+
+	}
+	
+	// Update is called once per frame
+
+	void FixedUpdate(){
+		WalkHandler();
+		JumpHandler();
+	}
+
+	void WalkHandler ()
+	{
+		//input on x horizonatl axis
+		float hAxis = Input.GetAxis("Horizontal");
+		//input on z vertical axis
+		float vAxis = Input.GetAxis("Vertical");
+
+		//movement in x
+		float moveX = transform.position.x + hAxis * walkSpeed * Time.deltaTime;
+
+		//movement in z
+		float moveZ = transform.position.z + vAxis * walkSpeed * Time.deltaTime;
+
+		//movement vector3 no jump
+		movement = new Vector3(moveX, transform.position.y, moveZ);
+		playerRB.MovePosition(movement);
+	}
+
+	void JumpHandler ()
+	{
+		float yAxis = Input.GetAxis ("Jump");
+
+		float moveY = yAxis * jumpSpeed;
+
+		Vector3 jumpVector = new Vector3 (0, moveY, 0);
+		if (yAxis > 0) {
+			if (canJump && isGrounded) {
+				playerRB.AddForce (jumpVector, ForceMode.VelocityChange);
+				canJump = false;
+				Debug.Log("hi");
+				isGrounded = false;
+			} else {
+				canJump = true;
+			}
+		}
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		isGrounded = true;
+	}
+
+	void OnTriggerEnter(Collider other){
+		Destroy(other.gameObject);
+	}
+}
